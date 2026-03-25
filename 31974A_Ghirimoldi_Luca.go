bd1c5ab0a9210ae13e772674de3b85bda978d996
Luca <luca.ghir04@gmail.com>
2026-03-25 16:48:38 +0100

    optimized input parsing

--- 31974A_Ghirimoldi_Luca.go
+++ 31974A_Ghirimoldi_Luca.go
@@ -31,45 +31,29 @@ funzione che applica al piano rappresentato da p l’operazione associata dalla 
 specificato nella Tabella 1.
 */
 func esegui(p piano, s string) {
-	line := strings.Fields(s) //separo gli elementi della linea in base alla presenza di uno o più spazi
+	line := strings.Fields(s)
+	// Parsing numerico preventivo (fino a 5 argomenti, default 0 se non presenti o non validi)
+	nums := make([]int, 5)
+	for i := 1; i < len(line) && i <= 5; i++ {
+		nums[i-1], _ = strconv.Atoi(line[i])
+	}
 	if len(line) > 0 {
 		char := []rune(line[0])[0]
-
 		switch char {
 		case 's':
-			x, _ := strconv.Atoi(line[1])
-			y, _ := strconv.Atoi(line[2])
-
-			fmt.Println(p.Stato(x, y))
+			fmt.Println(p.Stato(nums[0], nums[1]))
 		case 'S':
-
 			p.Stampa()
 		case 'a':
-			a, _ := strconv.Atoi(line[1])
-			b, _ := strconv.Atoi(line[2])
-
-			p.Automa(a, b, line[3])
+			p.Automa(nums[0], nums[1], line[3])
 		case 'o':
-			a, _ := strconv.Atoi(line[1])
-			b, _ := strconv.Atoi(line[2])
-			c, _ := strconv.Atoi(line[3])
-			d, _ := strconv.Atoi(line[4])
-
-			p.Ostacolo(a, b, c, d)
-
+			p.Ostacolo(nums[0], nums[1], nums[2], nums[3])
 		case 'r':
-			a, _ := strconv.Atoi(line[1])
-			b, _ := strconv.Atoi(line[2])
-
-			p.Richiamo(a, b, line[3])
+			p.Richiamo(nums[0], nums[1], line[3])
 		case 'p':
 			fmt.Println(p.Posizioni(line[1]))
-
 		case 'e':
-			a, _ := strconv.Atoi(line[1])
-			b, _ := strconv.Atoi(line[2])
-
-			esiste := p.EsistePercorso(a, b, line[3])
+			esiste := p.EsistePercorso(nums[0], nums[1], line[3])
 			if esiste {
 				fmt.Println("SI")
 			} else {
@@ -77,10 +61,9 @@ func esegui(p piano, s string) {
 			}
 		case 'f':
 			os.Exit(0)
-		case 't': // per febbraio
-			fmt.Println(0)
+		case 't':
+			fmt.Println(0) // Placeholder per test
 		default:
-
 		}
 	}
 
